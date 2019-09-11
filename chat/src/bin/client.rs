073365bc92617feb69bc551b8a117087994ad86a
Eliza Weisman <eliza@buoyant.io>
2019-09-11 13:34:11 -0700

    de-implement chat client, add comments.
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- chat/src/bin/client.rs
+++ chat/src/bin/client.rs
@@ -1,10 +1,9 @@
 use structopt::StructOpt;
 
-use crossterm_style::{Colorize, Styler};
-use futures::{stream, Stream, Sink, SinkExt, StreamExt, TryStream, TryStreamExt};
+use futures::{Stream, Sink, SinkExt, StreamExt, TryStream, TryStreamExt};
 use std::net::SocketAddr;
 use tokio::{
-    codec::{FramedRead, FramedWrite, LinesCodec, LinesCodecError},
+    codec::{FramedRead, FramedWrite, LinesCodec},
     net::TcpStream,
 };
 
@@ -22,46 +21,51 @@ struct Options {
 
 type Error = Box<dyn std::error::Error>;
 
+/// Splits a `TcpStream` of raw bytes into a `Stream` of lines and a sink that
+/// we can write lines to.
 fn lines_from_conn(conn: TcpStream) -> (impl Stream<Item = Result<String, Error>>, impl Sink<String, Error = Error>) {
+    // Split `conn` into a read half and a write half, implementing the
+    // AsyncRead trait and the AsyncWrite trait, respectively.
     let (read, write) = conn.split();
 
-    let write_lines = FramedWrite::new(write, LinesCodec::new()).sink_err_into();
+    // `FramedRead` turns an `AsyncRead` into a `Stream` of _frames_ using a
+    // codec. When we read from the stream, the codec will asynchronously read
+    // from the `AsyncRead` into a buffer until it reads a newline, and then
+    // return the buffered frame.
     let read_lines = FramedRead::new(read, LinesCodec::new()).err_into();
+    // Similarly, a `FramedWrite` implements a `Sink` for a frame type by
+    // encoding each frame into bytes and writing those bytes to an
+    // `AsyncWrite`.
+    let write_lines = FramedWrite::new(write, LinesCodec::new()).sink_err_into();
+
     (read_lines, write_lines)
 }
 
+/// Returns a `Stream` of lines read from stdin.
 fn lines_from_stdin() -> impl Stream<Item = Result<String, Error>> {
     let stdin = tokio::io::stdin();
     FramedRead::new(stdin, LinesCodec::new()).err_into()
 }
 
-#[derive(Debug)]
-enum Event {
-    Received(String),
-    Input(String),
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse command line options.
     let Options { server, user } = Options::from_args();
 
-    let in_char = ">".bold().blue();
-
+    // Try to connect to the server, returning a `TcpStream`.
     let conn = TcpStream::connect(server).await?;
-    println!("{} connected to {}", in_char, server);
+    println!("connected to {}", server);
+
+    // Split the raw `TcpStream` of bytes into a `Stream` of received lines and
+    // a sink that we can write lines to send to.
     let (recv_lines, mut send_lines) = lines_from_conn(conn);
 
+    // Start by sending the server our username.
     send_lines.send(user).await?;
 
-    let stdin_lines = lines_from_stdin().map_ok(Event::Input);
-    let recv_lines = recv_lines.map_ok(Event::Received);
-    let mut events = stream::select(stdin_lines, recv_lines);
-
-    while let Some(event) = events.try_next().await? {
-        match event {
-            Event::Received(s) => println!("{} {}", in_char, s),
-            Event::Input(s) => send_lines.send(s).await?,
-        }
-    }
-    Ok(())
+    // Now you try! To finish implementing the client, we'll need to
+    // continuously read from stdin and from `recv_lines`. When we receive a
+    // line from stdin, we'll need to write that line to `send_lines`. When we
+    // receive a line from `read_lines`, we need to print that to stdout.
+    unimplemented!()
 }
