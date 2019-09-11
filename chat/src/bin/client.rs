2c6fa214bea9da3cfd16758a16e9da270fec03ec
Eliza Weisman <eliza@buoyant.io>
2019-09-11 13:21:27 -0700

    clean up client
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- chat/src/bin/client.rs
+++ chat/src/bin/client.rs
@@ -1,10 +1,10 @@
 use structopt::StructOpt;
 
 use crossterm_style::{Colorize, Styler};
-use futures::{stream, SinkExt, StreamExt, TryStreamExt};
+use futures::{stream, Stream, Sink, SinkExt, StreamExt, TryStream, TryStreamExt};
 use std::net::SocketAddr;
 use tokio::{
-    codec::{FramedRead, FramedWrite, LinesCodec},
+    codec::{FramedRead, FramedWrite, LinesCodec, LinesCodecError},
     net::TcpStream,
 };
 
@@ -20,9 +20,24 @@ struct Options {
     user: String,
 }
 
+type Error = Box<dyn std::error::Error>;
+
+fn lines_from_conn(conn: TcpStream) -> (impl Stream<Item = Result<String, Error>>, impl Sink<String, Error = Error>) {
+    let (read, write) = conn.split();
+
+    let write_lines = FramedWrite::new(write, LinesCodec::new()).sink_err_into();
+    let read_lines = FramedRead::new(read, LinesCodec::new()).err_into();
+    (read_lines, write_lines)
+}
+
+fn lines_from_stdin() -> impl Stream<Item = Result<String, Error>> {
+    let stdin = tokio::io::stdin();
+    FramedRead::new(stdin, LinesCodec::new()).err_into()
+}
+
 #[derive(Debug)]
 enum Event {
-    Recieved(String),
+    Received(String),
     Input(String),
 }
 
@@ -33,21 +48,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let in_char = ">".bold().blue();
 
     let conn = TcpStream::connect(server).await?;
-    println!("{} connected to {}", in_char, server,);
+    println!("{} connected to {}", in_char, server);
+    let (recv_lines, mut send_lines) = lines_from_conn(conn);
 
-    let (read, write) = conn.split();
-
-    let mut send_lines = FramedWrite::new(write, LinesCodec::new());
     send_lines.send(user).await?;
 
-    let stdin = tokio::io::stdin();
-    let stdin_lines = FramedRead::new(stdin, LinesCodec::new()).map_ok(Event::Input);
-    let recv_lines = FramedRead::new(read, LinesCodec::new()).map_ok(Event::Recieved);
+    let stdin_lines = lines_from_stdin().map_ok(Event::Input);
+    let recv_lines = recv_lines.map_ok(Event::Received);
     let mut events = stream::select(stdin_lines, recv_lines);
 
-    while let Some(event) = events.next().await {
-        match event? {
-            Event::Recieved(s) => println!("{} {}", in_char, s),
+    while let Some(event) = events.try_next().await? {
+        match event {
+            Event::Received(s) => println!("{} {}", in_char, s),
             Event::Input(s) => send_lines.send(s).await?,
         }
     }
